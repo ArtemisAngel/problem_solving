a60be7776b6adca211bc59046aedef5b1fd658ef
ArtemisAngel <angelhere2007@gmail.com>
2025-05-03 13:40:57 +0530

    Implement solution to remove duplicates from sorted list

---
+++ RemovingDuplicateInplacefromSortedArray.py
@@ -0,0 +1,21 @@
+#Time Complexity = O(n)
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+
+        for i in range (1,len(nums)):   # T(n) = O(n)
+            if nums[i]!=nums[k]:
+                k+=1
+                nums[k] = nums[i]
+
+        return k+1
+
+if __name__ == "__main__":
+    n = list(map(int, input().split(",")))
+    sol = Solution()
+    k = sol.removeDuplicates(n)
+    for i in range(k):
+        print(n[i], end=",")
