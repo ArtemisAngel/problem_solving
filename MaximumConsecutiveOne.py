d731d3aa6d8c6df9b75cf185701525d33ab8d8ec
ArtemisAngel <angelhere2007@gmail.com>
2025-05-14 17:12:18 +0530

    Initial Commit

---
+++ MaximumConsecutiveOne.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        rec = 0
+        count = 0
+        for i in nums:
+            if i==1:
+                count+=1
+            else:
+                count = 0
+            if count>rec:
+                rec = count
+        return rec
+
+if __name__ == "__main__":
+    obj = Solution()
+    arr = list(map(int, input("Space Separated Values: ").split(" ")))
+    res = obj.findMaxConsecutiveOnes(arr)
+    print(res)
+    
