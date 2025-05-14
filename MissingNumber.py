8e0908f58ae30d15ad39a2997224d5e75bfb0277
ArtemisAngel <angelhere2007@gmail.com>
2025-05-14 23:52:32 +0530

    Solution to find the Missing Number from a list

---
+++ MissingNumber.py
@@ -0,0 +1,14 @@
+# T(O) = N
+
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        sum1 = (n*(n+1))/2 
+        sum2 = sum(nums) #O(n)
+        return sum2-sum1
+
+if __name__=="__main__":
+    obj = Solution()
+    arr = list(map(int, input("Space Separated Values: ").split(" ")))
+    res = obj.missingNumber(arr)
+    print(res)
